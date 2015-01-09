de04a13b98eeab7b3630233f4dd520e9770456ea
Jonas Maaskola <jonas@maaskola.de>
2015-01-09 03:08:44 +0100

    Add package version

--- scripts/make-debian-package.sh
+++ scripts/make-debian-package.sh
@@ -3,9 +3,19 @@
 # Run it from the root directory of the Discrover package.
 # It uses the current branch to generate a source archive.
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "One argument required, $# provided"
+echo $1 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $1 provided"
+
+
 DIR=$( cd "$( dirname "$0" )" && pwd )/..
 export VERSION=`git describe | sed -e "s/-/./g"`
 echo $VERSION
+export PACKAGE_VERSION=$VERSION-$1
 
 ruby scripts/make-source-release.rb $DIR/deb/discrover_$VERSION.tar.gz
 
@@ -16,5 +26,5 @@ tar xf discrover_$VERSION.orig.tar.gz
 # the debian directory contains some necessary files
 cp -r debian/ discrover-$VERSION/
 cd discrover-$VERSION/
-dch --create -v $VERSION -M --package discrover
+dch --create -v $PACKAGE_VERSION -M --package discrover
 debuild -us -uc
